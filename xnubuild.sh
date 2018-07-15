7b7a3fd3c0cfd7c068d8178e492d7aa9bb5be4e8
William Kent <wjk@users.noreply.github.com>
2018-07-15 12:58:09 -0400

    Fix libfirehose build

--- xnubuild.sh
+++ xnubuild.sh
@@ -175,7 +175,10 @@ print "Setting up libfirehose"
 	cd $SCRIPT_DIRECTORY/$LIBDISPATCH_VERSION && \
 		patch -s -p1 < $PATCH_DIRECTORY/libfirehose/header-paths.patch && \
 		patch -s -p1 < $PATCH_DIRECTORY/libfirehose/missing-xcconfig.patch && \
-		xcodebuild install -project libdispatch.xcodeproj -target libfirehose_kernel -sdk macosx ARCHS='x86_64' SRCROOT=$PWD OBJROOT=$BUILD_DIR/$LIBDISPATCH_VERSION.obj SYMROOT=$BUILD_DIR/$LIBDISPATCH_VERSION.sym DSTROOT=$BUILD_DIR/$LIBDISPATCH_VERSION.dst ADDITIONAL_SDKS=$BUILD_DIR/dependencies DEPENDENCIES_DIR=$BUILD_DIR/dependencies && \
+		patch -s -p1 < $PATCH_DIRECTORY/libfirehose/no-werror.patch && \
+		patch -s -p1 < $PATCH_DIRECTORY/libfirehose/void-returns-void.patch && \
+		patch -s -p1 < $PATCH_DIRECTORY/libfirehose/include-standard-path.patch && \
+		xcodebuild install -project libdispatch.xcodeproj -target libfirehose_kernel -sdk macosx ARCHS='x86_64' SRCROOT=$PWD OBJROOT=$BUILD_DIR/$LIBDISPATCH_VERSION.obj SYMROOT=$BUILD_DIR/$LIBDISPATCH_VERSION.sym DSTROOT=$BUILD_DIR/$LIBDISPATCH_VERSION.dst DEPENDENCIES_DIR=$BUILD_DIR/dependencies && \
 		ditto $BUILD_DIR/$LIBDISPATCH_VERSION.dst/usr/local $BUILD_DIR/dependencies/usr/local
 } || {
 	error "Failed to setup libfirehose"
