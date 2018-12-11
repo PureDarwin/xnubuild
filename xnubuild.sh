882c3454a12ac54938ab638801d755ac1194d61c
William Kent <wjk@users.noreply.github.com>
2018-12-11 15:13:30 -0500

    Squelch pointless output

--- xnubuild.sh
+++ xnubuild.sh
@@ -131,7 +131,7 @@ print "Building dtrace"
 {
 	mkdir -p $BUILD_DIR/$DTRACE_VERSION.{obj,sym,dst}
 	cd $SCRIPT_DIRECTORY/$DTRACE_VERSION && \
-		patch -p1 < $PATCH_DIRECTORY/dtrace/header-paths.patch && \
+		patch -s -p1 < $PATCH_DIRECTORY/dtrace/header-paths.patch && \
 		xcodebuild install -target ctfconvert -target ctfdump -target ctfmerge ARCHS="x86_64" SRCROOT=$PWD OBJROOT=$BUILD_DIR/$DTRACE_VERSION.obj SYMROOT=$BUILD_DIR/$DTRACE_VERSION.sym DSTROOT=$BUILD_DIR/$DTRACE_VERSION.dst && \
 		ditto $BUILD_DIR/$DTRACE_VERSION.dst/$XCODE_DEVELOPER_DIR/Toolchains/XcodeDefault.xctoolchain $BUILD_DIR/dependencies
 } || {
